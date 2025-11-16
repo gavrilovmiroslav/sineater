8320ef86d30689dfb4e87fa35a6dea27f15731de
Isa1997 <stefan.i@hotmail.rs>
2025-11-16 18:57:38 +0000

    Guess who is pressing buttons now

---
+++ Input/InputContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Input;
+using SINEATER.Content;
+using System.Collections.Generic;
+
+namespace SINEATER.Input
+{
+    internal abstract class IInputDefinition
+    {
+        public EInputActions InputAction { get; set; }
+
+        public abstract bool IsPressed();
+        public abstract bool IsReleased();
+    }
+
+    internal class SingleInputDefinition : IInputDefinition
+    {
+        private Keys Key = Keys.None;
+        public override bool IsPressed()
+        {
+            return KB.IsPressed(Key);
+        }
+
+        public override bool IsReleased()
+        {
+            return KB.IsReleased(Key);
+        }
+    }
+
+    internal class InputContext
+    {
+        public string Name = "";
+        public List<IInputDefinition> Inputs = new();
+    }
+}
---
+++ Input/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+
+namespace SINEATER.Input
+{
+    internal class InputManager
+    {
+        public static InputManager Instance = new();
+
+        private Dictionary<string, InputContext> _loadedContexts = new();
+
+        private Stack<InputContext> InputStacks = new();
+
+        public void Initialize(ContentManager contentManager)
+        {
+            // load json
+        }
+
+        public bool IsActionActive(EInputActions action)
+        {
+            var context = InputStacks.Peek();
+            if (context != null)
+            {
+                var definition = context.Inputs.Find(x => x.InputAction == action);
+                return true;
+                //definition.
+            }
+            return false;
+        }
+
+        public void PushContext(string contextName)
+        {
+            if (_loadedContexts.ContainsKey(contextName))
+            {
+                InputStacks.Push(_loadedContexts[contextName]);
+            }
+        }
+
+        public void PopContext()
+        {
+            InputStacks.Pop();
+        }
+    }
+}
