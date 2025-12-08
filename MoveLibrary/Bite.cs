c7cee457077037e2afc27f7bbfff016248670f4e
Miroslav <gavrilov.miroslav@gmail.com>
2025-12-08 01:46:32 +0100

    Made Bite deal 1 damage

--- MoveLibrary/Bite.cs
+++ MoveLibrary/Bite.cs
@@ -5,14 +5,25 @@ namespace SINEATER.MoveLibrary;
 [Move]
 public class Bite : Move
 {
+    IEnumerable Attack(Character atk, Attack attack, Character def, CombatMapScreen screen)
+    {
+        def.HP -= 1;
+        if (def.HP <= 0)
+        {
+            def.Die();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+    }
+    
     public override string Name { get; } = "Bite";
     public override string Description { get; } = "";
     public override EMoveCost[] Costs { get; } = [];
 
     protected override IEnumerable MoveAction(Character character, CombatMapScreen screen)
     {
-        character.MovementLeft = 3;
-        character.Attacks.Add(new Attack([], []));
+        character.MovementLeft = 1;
+        character.Attacks.Add(new Attack([], [], new StatsScaling(), Attack));
         yield break;
     }
 }
