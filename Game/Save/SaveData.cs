2dddf9023a022b0ec49246725d53ad84eab06fd6
Isa1997 <stefan.i@hotmail.rs>
2026-01-22 22:09:02 +0000

    add save game data

---
+++ Game/Save/SaveData.cs
@@ -0,0 +1,28 @@
+﻿using SINEATER.Game.CoreUtils;
+using System.IO;
+
+namespace SINEATER.Game.Save
+{
+    public static class SaveSystem
+    {
+        public static void Save()
+        {
+            DataSerializer.Serialize(SineaterGame.Instance.Party, out string json);
+
+            if (!Directory.Exists("Save"))
+                Directory.CreateDirectory("Save");
+
+            File.WriteAllText("save//save.sav", json);
+        }
+
+        public static void Load(string path)
+        {
+
+        }
+    }
+
+    public class SaveData
+    {
+
+    }
+}
