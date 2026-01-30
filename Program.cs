ebe77521d83e827d534e2dc2421e9c22c5a162c3
Isa1997 <stefan.i@hotmail.rs>
2026-01-30 23:36:07 +0000

    Primitive crash reporting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
 ﻿using SINEATER.Game;
+using System;
+using System.IO;
 
-using var game = new SineaterGame();
-game.Run();
+try
+{
+    using var game = new SineaterGame();
+    game.Run();
+}
+catch (Exception e)
+{
+    if (!Directory.Exists("Crash"))
+    {
+        Directory.CreateDirectory("Crash");
+    }
+
+    var timeInfo = DateTime.Now;
+    var fileName = $"Crash\\crash_{timeInfo.Day}-{timeInfo.Month}-{timeInfo.Year}-{timeInfo.Hour}-{timeInfo.Minute}.txt";
+    using StreamWriter w = new StreamWriter(fileName);
+
+    w.Write(e.StackTrace);
+    w.Flush();
+    w.Close();
+
+    Environment.Exit(1);
+}
